11525026219e3c6810c4182ac9d5f5d4f8b950d0
Nikhil.Adhau <Nikhil.Adhau@veritas.com>
2023-01-24 14:39:11 +0530

    added autocomplete, ctag plugins

--- vim.sh
+++ vim.sh
@@ -7,6 +7,9 @@ OS=`grep ID /etc/os-release -m 1 | cut -d "=" -f2 | tr -d '"'`
 if [ "$OS" = "rhel" ]
 then
 	FL="/etc/vimrc"
+	sudo yum install ctags
+else
+	sudo apt-get install exuberant-ctags
 fi
 
 #check if relative number is set
@@ -40,7 +43,7 @@ curl -Lo $C_DIR/onedark.vim https://raw.githubusercontent.com/joshdick/onedark.v
 curl -Lo "${DIR}/autoload/onedark.vim" https://raw.githubusercontent.com/joshdick/onedark.vim/main/autoload/onedark.vim
 
 #set default colorscheme
-CLR="onedark"
+CLR="molokai"
 #CH1=`grep "colorscheme ${CLR}" /etc/vim/vimrc`
 
 echo "colorscheme ${CLR}" | sudo tee -a ${FL}
@@ -57,10 +60,38 @@ fi
 
 #fugitive
 P_DIR_2="${P_DIR}/fugitive"
-if [[ ! -d $P_DIR_1 ]]
+if [[ ! -d $P_DIR_2 ]]
 then
 	git clone https://github.com/tpope/vim-fugitive.git $P_DIR_2
 fi
+
+#supertab
+P_DIR_3="${P_DIR}/supertab"
+if [[ ! -d $P_DIR_3 ]]
+then
+	git clone  https://github.com/ervandew/supertab.git  $P_DIR_3
+fi
+
+
+#nerdtree
+P_DIR_4="${P_DIR}/nerdtree"
+if [[ ! -d $P_DIR_4 ]]
+then
+	git clone  https://github.com/preservim/nerdtree.git  $P_DIR_4
+fi
+echo -e "map <silent> <C-g> :NERDTreeToggle<CR>" | sudo tee -a ${FL}
+
+
+#taglist
+P_DIR_5="${P_DIR}/taglist"
+if [[ ! -d $P_DIR_5 ]]
+then
+	git clone  https://github.com/preservim/tagbar.git  $P_DIR_5
+fi
+echo -e "map <silent> <C-t> :TagbarToggle<CR>" | sudo tee -a ${FL}
+
+
+
 
 
 
